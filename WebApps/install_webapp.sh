95bd422e5eb5e7228ab36010f70a5048efd60bda
Luc Kadletz <luckadletz@gmail.com>
2020-08-03 23:04:41 -0700

    Fixed webapp installer :)

--- WebApps/install_webapp.sh
+++ WebApps/install_webapp.sh
@@ -1,45 +1,53 @@
 #!/bin/bash
 
+ARCH="linux-x64" # Should match folder name pattern in APP_BUILD_DIR
+
 install-webapp(){
-	BUILD_DIR=$1 
+	# The full path of the built app (First Arg)
+	APP_BUILD_DIR=$1
+	# The path that will contain all of our published webapps (Second Arg) 
 	PUB_DIR=$2
-	APP_NAME=$3
-
-	echo "
-===	Installing $APP_NAME	===
-"
-
-	echo "Finding $APP_NAME in $BUILD_DIR"
-	APP_BUILD_DIR=$(find $BUILD_DIR -type d -name $APP_NAME\*)
-	echo "Found $APP_BUILD_DIR"
-
-	APP_PUB_DIR=$PUB_DIR/$(basename $APP_BUILD_DIR)
-
-	echo "Publishing $APP_BUILD_DIR to $APP_PUB_DIR"
-	if test -d $APP_PUB_DIR; then
-		sudo rm -r $APP_PUB_DIR
-	fi
-	sudo cp -r $APP_BUILD_DIR $APP_PUB_DIR
-
+	# The plain name of the app - found by stripping it from the build dir name
+	APP_NAME=`echo $APP_BUILD_DIR | sed -r "s/.*\/(.+)-$ARCH/\1/g"`
+	# The path of the app once it's published (we hope)
+	APP_PUB_DIR=$PUB_DIR/$APP_NAME-$ARCH
+	# The final app binary path (we hope)
+	APP_BINARY=$APP_PUB_DIR/$APP_NAME
+	# The pesky path to chrome sandbox we have to chown/chmod
 	SANDBOX_PATH=$APP_PUB_DIR/chrome-sandbox
-	echo "Setting permissions for $SANDBOX_PATH"
+	# The path to the icon file
+	ICON_PATH=$APP_PUB_DIR/resources/app/icon.png
+	# The path of the desktop file
+	DESKTOP_PATH=~/.local/share/applications/$APP_NAME.desktop
+
+	echo "=== Installing $APP_NAME from $APP_BUILD_DIR -> $APP_PUB_DIR ==="
+
+	echo "Publishing $APP_BUILD_DIR -> $PUB_DIR"
+
+	sudo mkdir -p $PUB_DIR
+	sudo cp -rf $APP_BUILD_DIR $PUB_DIR
+
+	echo "=== Setting permissions for $SANDBOX_PATH ==="
 	echo chown root $SANDBOX_PATH
 	sudo chown root $SANDBOX_PATH
 	echo chmod 4755 $SANDBOX_PATH
 	sudo chmod 4755 $SANDBOX_PATH
 
-	DESKTOP_PATH=$APP_PUB_DIR/$APP_NAME.desktop
-	echo "Copying desktop shortcut $DESKTOP_PATH"
-	if ! cp $DESKTOP_PATH ~/.local/share/applications/$APP_NAME.desktop; then
-		echo "copying desktop shortcut failed!"
-	fi
 
-	if command -v $APP_NAME; then
-		echo "$APP_NAME was already installed, skipping shortcut creation in /usr/bin"
-	else
-		echo "Creating shortcut in /usr/bin/$APP_NAME"
-		sudo ln -s $APP_PUB_DIR/$APP_NAME /usr/bin/$APP_NAME
-	fi
+	echo "=== Hacking together a desktop entry at $DESKTOP_PATH ==="
+	echo "[Desktop Entry]
+Type=Application
+Name=$APP_NAME
+Exec=$APP_BINARY
+Icon=$ICON_PATH
+StartupNotify=true
+Terminal=false
+Categories=Internet
+" > $DESKTOP_PATH
+
+
+	echo "=== Creating shortcut in /usr/bin/$APP_NAME ==="
+	sudo ln -sf $APP_PUB_DIR/$APP_NAME /usr/bin/$APP_NAME
 }
 
 
